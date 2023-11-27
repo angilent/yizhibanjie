3c84b29c06ea8cb02f195da3c5c415ccb559d215
Sean Shao <yue.shao@gmail.com>
2023-11-27 20:00:42 +0800

    添加 deploy 配置
    添加 网站baseurl 为/yizhibanjie

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  website: "https://angilent.github.io/", // replace this with your deployed domain
   author: "邵悦",
   desc: "欢迎来到悦的兔子洞",
   title: "一只半解",
